6261daa1631008e6609e711891e99838750c7988
elik <akainooo@gmail.com>
2022-08-01 01:15:51 +0800

    feature: add rspec & rubocop config

--- config/application.rb
+++ config/application.rb
@@ -2,21 +2,19 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Caato
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = "Taipei"
+    config.i18n.default_locale = 'zh-TW'
+    config.active_record.default_timezone = :local
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    if ['development', 'test'].include? ENV['RAILS_ENV']
+      Dotenv::Railtie.load
+    end
+    
+    HOSTNAME = ENV['HOSTNAME']
   end
 end
