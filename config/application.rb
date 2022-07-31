2103142b7612ece90885a55147b02bf1e155a8d1
elik <akainooo@gmail.com>
2022-08-01 01:57:29 +0800

    feature: add simple auth

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,8 @@ Bundler.require(*Rails.groups)
 module Caato
   class Application < Rails::Application
     config.load_defaults 6.1
-    config.time_zone = "Taipei"
-    config.i18n.default_locale = 'zh-TW'
+    config.time_zone = 'Taipei'
+    # config.i18n.default_locale = 'zh-TW'
     config.active_record.default_timezone = :local
 
     if ['development', 'test'].include? ENV['RAILS_ENV']
